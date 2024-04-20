5e095c236c5dd3d1b1fe21e8ff228258650a7fe1
definfo <syx_definfo@sjtu.edu.cn>
2024-04-20 15:19:18 +0800

    my solition for util lab exercise sleep

---
+++ user/sleep.c
@@ -0,0 +1,22 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "kernel/fcntl.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  if (argc <= 1){
+    fprintf(2, "Usage: sleep time...\n");
+    exit(1);
+  }
+
+  if (argc > 2) {
+    fprintf(2, "sleep: failed to sleep\n");
+    exit(1);
+  }
+  
+  int t = atoi(argv[1]);
+  sleep(t);
+  exit(0);
+}
