b8279798a8a5b59279681e62efd66fdf984f79a0
definfo <hjsdbb1@gmail.com>
2024-10-05 15:45:16 +0800

    feat: util/xargs

--- user/xargs.c
+++ user/xargs.c
@@ -1,67 +1,87 @@
-#include <kernel/param.h>
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
 
-void exec_cmd(const char* cmd, char* args[])
+#define MAXARG 14
+
+char* readline()
 {
-    if (fork() == 0)
+    char* buf = malloc(512);
+    char* p = buf;
+    while (read(0, p, 1) == 1)
     {
-        exec(cmd, args); // 1
-        exit(0);
+        switch (*p)
+        {
+        case '\0':
+        case '\n':
+            *p = '\0'; // End of string
+            return buf;
+        default:
+            p++;
+            break;
+        }
     }
-    else
-    {
-        wait(0);
-    }
+    if (p != buf) return buf;
+    free(buf);
+    return 0;
 }
 
 int main(int argc, char* argv[])
 {
     if (argc < 2)
     {
-        fprintf(2, "Usage: xargs [command] ...");
+        fprintf(2, "Usage: xargs [command] ...\n");
         exit(1);
     }
 
     char* args_buf[MAXARG];
 
     char** ptr_xargs = args_buf;
-    for (int i = 2; i < argc; i++)
+    for (int i = 1; i < argc; i++)
     {
-        printf("%s\n", argv[i]);
         *ptr_xargs++ = argv[i];
     }
-    char** ptr_args = ptr_xargs;
 
-    char buf[512];
-    char* buf_start = buf;
-    char* buf_end = buf;
-    while (read(0, buf_start, 1) == 1)
+    char* l;
+    // Read line
+    while ((l = readline()) != 0)
     {
-        switch (*buf_start)
+        char* ptr_l = l;
+        char* buf = malloc(512);
+        char* ptr_buf = buf;
+        int arg_cnt = argc - 1;
+
+        // Parse arguments from line
+        while (*ptr_l != 0)
         {
-        case ' ':
-            *buf_start = '\0'; // End of argument
-            *ptr_args++ = buf_end; // Add arg into args_buf
-            buf_end = buf_start + 1;
-        case '\n':
-            *ptr_args = 0; // Change line
-            exec_cmd(argv[1], args_buf);
-            ptr_args = ptr_xargs; // Reset ptr_args
-            break;
-        default:
-            buf_start++;
-            break;
+            if (*ptr_l == ' ' && buf != ptr_buf)
+            {
+                *ptr_buf = 0;
+                args_buf[arg_cnt++] = buf;
+                buf = malloc(512);
+                ptr_buf = buf;
+            }
+            else
+            {
+                *ptr_buf++ = *ptr_l;
+            }
+            ptr_l++;
         }
-    }
-    // Last line
-    if (ptr_args != ptr_xargs)
-    {
-        *buf_start = '\0'; // End of argument
-        *ptr_args++ = buf_end; // Add arg into args_buf
-        *ptr_args = 0; // Change line
-        exec_cmd(argv[1], args_buf);
+        if (buf != ptr_buf)
+        {
+            args_buf[arg_cnt++] = buf;
+        }
+        args_buf[arg_cnt] = 0;
+        free(l);
+
+        if (fork() == 0)
+        {
+            exec(args_buf[0], args_buf);
+        }
+        else
+        {
+            wait(0);
+        }
     }
     exit(0);
 }
